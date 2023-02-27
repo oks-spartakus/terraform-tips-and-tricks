48cd91a2008ccd337920d7ecf6419446d209655b
oks-spartakus <oks.spartakus@gmail.com>
2023-02-27 16:07:02 +0100

    added foreach

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -6,3 +6,10 @@ resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
 }
+
+# or
+
+resource "aws_iam_user" "foreach-example" {
+  for_each = toset(var.user_names)
+  name     = each.value
+}
--- live/global/iam/outputs.tf
+++ live/global/iam/outputs.tf
@@ -6,5 +6,16 @@ output "spongebob_arn" {
 output "all_arns" {
   value       = aws_iam_user.example[*].arn
   description = "ARNs of all users"
+}
+
+# or
+
+output "all_users_foreach" {
+  value       = aws_iam_user.foreach-example
+  description = "users"
+}
+
+output "all_arns" {
+  value = values(aws_iam_user.foreach-example)[*].arn
 
 }
